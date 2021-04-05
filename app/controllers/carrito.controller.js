fb31ec82f269d1e0b6b4a67686ce76ba05530a0f
ymk <frostmour2013@gmail.com>
2021-04-05 18:53:59 -0400

    working in cart details

--- app/controllers/carrito.controller.js
+++ app/controllers/carrito.controller.js
@@ -2,6 +2,7 @@
 const db = require("../models");
 const Carrito = db.carrito;
 const Curso = db.cursos;
+const User = db.user;
 const Op = db.Sequelize.Op;
 
 
@@ -9,21 +10,50 @@ const Op = db.Sequelize.Op;
 exports.create = (req, res) => {
   
   // Create a curso
-  const carrito = {
-    descripcion: req.body.userId,
-  };
 
-  // Save curso in the database
-  Carrito.create(carrito)
-    .then(data => {
-      res.send(data);
+  User.findByPk(req.body.userId)
+    .then(userDB => {
+     
+
+      Carrito.findAll({
+        limit: 1,
+        where: {
+          //your where conditions, or without them if you need ANY entry
+          userId: userDB.id
+        },
+        order: [['createdAt', 'DESC']]
+      }).then(function (carritos) {
+
+        //only difference is that you get users list limited to 1
+        if(carritos.length > 0){
+          res.send(carritos[0]);
+        }else{
+          // Save carrito in the database
+          const carrito = {
+            userId: userDB.id,
+          };
+          Carrito.create(carrito)
+            .then(carritoDB => {
+              userDB.setCarrito(carritoDB);
+              res.send(carritoDB);
+            })
+            .catch(err => {
+              res.status(500).send({
+                message:
+                  err.message || "Some error occurred while creating the carrito."
+              });
+            });
+        }
+      });        
     })
-    .catch(err => {
+    .catch (err => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the curso."
+        message: "Error finding user " + err.message
       });
-    });
+  });
+  
+
+  
 };
 
 
@@ -45,18 +75,40 @@ exports.addCurso = (req, res) => {
                  console.log(
                    `>> added Curso id=${cursoDB.id} to Carrito id=${carritoDB.id}`
                  );
-                 return carritoDB;
+              res.send(carritoDB);
             })
             .catch(err => {
                 res.status(500).send({
-                    message: "Error retrieving curso with id=" + idCurso
+                  message: "Error finding curso" + err.message
                 })
             });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error finding carrito " + err.message
+      });
+    });
+};
+
+
+// Find a single cart with cursos
+exports.getCursos = (req, res) => {
+  const id = req.params.id;
+
+  Carrito.findAll({
+    where: {
+      id: id
+    },
+    include: [
+      { model: Curso, as: Curso.tableName }
+    ]
+  } )
+    .then(data => {
       res.send(data);
     })
     .catch(err => {
       res.status(500).send({
-        message: "Error retrieving carrito with id=" + idCarrito
+        message: "Error retrieving cart with id=" + id
       });
     });
 };
--- app/routes/carrito.routes.js
+++ app/routes/carrito.routes.js
@@ -3,12 +3,17 @@ module.exports = (app) => {
 
   var router = require("express").Router();
 
-  // Create a new curso
+  // Create a new cart
   router.post("/", carrito.create);
   //Add curso to cart
   router.post("/add-curso", carrito.addCurso);
+  //Get cursos in cart
+
+  router.get("/get-cursos/:id", carrito.getCursos);
+
 
   app.use("/api/carrito", router);
+  
 
   
 };
